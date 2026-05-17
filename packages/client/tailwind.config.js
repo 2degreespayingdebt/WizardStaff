7c0c7507cba4b4fe2392dfc2c8b8fa7a436c8ba2
Steven B <stevenb@mac.lan>
2026-05-16 20:18:47 -0400

    Beach blue and sand color theme

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ocean: {
+          900: '#023E8A',
+          800: '#0077B6',
+          700: '#00B4D8',
+          600: '#48CAE4',
+          500: '#90E0EF',
+        },
+        sand: {
+          800: '#B8956A',
+          700: '#D4A574',
+          600: '#E8D4B8',
+          500: '#FAF9F6',
+        },
+      },
+    },
   },
   plugins: [],
 }
